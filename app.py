1892fb1bc79cdc9155a04237cef941c27a2c5a70
KInashima <kunikazu.inashima@gmail.com>
2023-03-09 22:43:54 -0500

    app.py updated and tested

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ import streamlit as st
 import pandas as pd
 import plotly_express as px
 
+st.header('Sprint 4: SDA US Vehicles Analysis')
+
+veh_info_df=pd.read_csv(r'vehicles_us.csv')
+
+st.plotly_chart(fig_dom_time, theme=None, use_container_width=True)
+st.plotly_chart(fig_int_time, theme=None, use_container_width=True)
+
+st.plotly_chart(fig_dom_price, theme=None, use_container_width=True)
+st.plotly_chart(fig_int_price, theme=None, use_container_width=True)
